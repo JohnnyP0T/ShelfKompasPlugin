8955d8905c49100732232d1bab8b6c2cf6d70877
Johnny pot <arroga077@gmail.com>
2022-12-09 01:35:12 +0700

    stress testing

--- src/Tests/StessTesting/Program.cs
+++ src/Tests/StessTesting/Program.cs
@@ -8,46 +8,32 @@ using System;
 using System.IO;
 using System.Globalization;
 using System.Xml.Linq;
+using InventorApi;
 
-Console.WriteLine("Hello, World!");
 var builder = new ShelfBuilder();
-var stopWatch = new Stopwatch();
-var apiService = new KompasWrapper();
-stopWatch.Start();
+var apiService = new InventorWrapper();
 var shelfParameters = new ShelfParameters();
 var streamWriter = new StreamWriter($"log{apiService}.txt", true);
-var process = Process.GetCurrentProcess();
 
-
-// Define variables to track the peak
-// memory usage of the process.
 long peakPagedMem = 0,
     peakWorkingSet = 0,
     peakVirtualMem = 0,
     countDetal = 1;
 builder.BuildShelf(shelfParameters, apiService);
-// Start the process.
-//Process myProcess1 = Process.GetProcessesByName("kStudy").FirstOrDefault();
-using (Process myProcess = Process.GetProcessesByName("kStudy").FirstOrDefault())
+
+using (Process myProcess = Process.GetProcessesByName("Inventor").FirstOrDefault())
 {
-    // Display the process statistics until
-    // the user closes the program.
+   
     do
     {
         if (!myProcess.HasExited)
         {
             builder.BuildShelf(shelfParameters, apiService);
             countDetal++;
-            // Refresh the current process property values.
             myProcess.Refresh();
-
             Console.WriteLine();
-
-            // Display current process statistics.
-
             Console.WriteLine($"{myProcess} -");
             Console.WriteLine("-------------------------------------");
-
             Console.WriteLine($"  Количество деталей        : {countDetal}");
             Console.WriteLine($"  Physical memory usage     : {myProcess.WorkingSet64}");
             Console.WriteLine($"  Base priority             : {myProcess.BasePriority}");
@@ -57,8 +43,8 @@ using (Process myProcess = Process.GetProcessesByName("kStudy").FirstOrDefault()
             Console.WriteLine($"  Total processor time      : {myProcess.TotalProcessorTime}");
             Console.WriteLine($"  Paged system memory size  : {myProcess.PagedSystemMemorySize64}");
             Console.WriteLine($"  Paged memory size         : {myProcess.PagedMemorySize64}");
-
-            // Update the values for the overall peak memory statistics.
+            streamWriter.WriteLine($"{countDetal} {myProcess.WorkingSet64} {myProcess.UserProcessorTime}");
+            streamWriter.Flush();
             peakPagedMem = myProcess.PeakPagedMemorySize64;
             peakVirtualMem = myProcess.PeakVirtualMemorySize64;
             peakWorkingSet = myProcess.PeakWorkingSet64;
@@ -73,12 +59,11 @@ using (Process myProcess = Process.GetProcessesByName("kStudy").FirstOrDefault()
             }
         }
     }
-    while (!myProcess.WaitForExit(1000));
+    while (countDetal != 200);
 
     Console.WriteLine();
     Console.WriteLine($"  Process exit code          : {myProcess.ExitCode}");
 
-    // Display peak memory statistics for the process.
     Console.WriteLine($"  Peak physical memory usage : {peakWorkingSet}");
     Console.WriteLine($"  Peak paged memory usage    : {peakPagedMem}");
     Console.WriteLine($"  Peak virtual memory usage  : {peakVirtualMem}");
